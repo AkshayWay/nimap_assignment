78abff17588690c47b7d6b36aca537bb28b0054f
akshay.waingankar95@gmail.com <akshay.waingankar95@gmail.com>
2020-09-22 00:48:17 +0530

    Add and delete task completed

--- src/components/task.js
+++ src/components/task.js
@@ -1,9 +1,10 @@
 import React, { component, Component } from "react";
 import { Modal, Button, Form, Col, Alert } from "react-bootstrap";
 import axios from "axios";
-var items = [];
+import $ from "jquery";
+
 const TaskList = (props) => (
-  <tr>
+  <tr id={props.TaskInfo.id}>
     <td>{props.TaskInfo.id}</td>
     <td>{props.TaskInfo.title}</td>
 
@@ -12,27 +13,21 @@ const TaskList = (props) => (
     </td>
 
     <td>
-      <DeleteNewsInfo variant={props.TaskInfo} />
+      <DeleteTaskInfo variant={props.TaskInfo} />
     </td>
   </tr>
 );
 
-function DeleteNewsInfo(props) {
+function DeleteTaskInfo(props) {
   const [show, setShow] = React.useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  console.log("props.TaskInfo:" + props.variant.title);
   const deleteAndClose = () => {
     var retrievedData = localStorage.getItem("LocalDataArr");
     var items = JSON.parse(localStorage.getItem("LocalDataArr"));
-
-    // items = items.filter((item) => item.id == props.variant.id);
-    // alert("Length:" + items.length);
-    // if (items.length !== 0) {
-    //   localStorage.removeItem("LocalDataArr");
-    // }
-    // console.log("new data", localStorage.getItem("LocalDataArr"));
-
+    //Delete row from table
+    $("table#taskTbl tr#" + props.variant.id + "").remove();
+    //Delete row from localstorage
     for (var i = 0; i < items.length; i++) {
       var item = items[i];
       if (item.id == props.variant.id) {
@@ -41,12 +36,12 @@ function DeleteNewsInfo(props) {
     }
     items = JSON.stringify(items);
     localStorage.setItem("LocalDataArr", items);
-    console.log("New data", localStorage.getItem("LocalDataArr"));
+
     setShow(false);
   };
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
+      <Button variant="primary" className="btn btn-danger" onClick={handleShow}>
         Delete
       </Button>
 
@@ -69,7 +64,67 @@ function DeleteNewsInfo(props) {
     </>
   );
 }
+function AddNewTask() {
+  const [show, setShow] = React.useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const AddAndClose = () => {
+    var rowCount = $("#taskTbl tr").length;
+    var isCompleted = $("#isCompleted").is(":checked");
+    var newTitle = $("#newTitle").val();
+    setShow(false);
+    $("#taskTbl").append(
+      "<tr id='rowCount'><td>" +
+        rowCount +
+        "</td><td>" +
+        newTitle +
+        "</td><td>" +
+        isCompleted +
+        "</td><td><button type='button' class='btn btn-primary'>Delete</button></td></tr>"
+    );
+  };
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Add Task
+      </Button>
 
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Completed</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <input type="text" id="newTitle" />
+                </td>
+                <td>
+                  <input type="checkbox" id="isCompleted" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={AddAndClose}>
+            Add
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 export default class task extends Component {
   constructor(props) {
     super(props);
@@ -84,17 +139,8 @@ export default class task extends Component {
       });
       console.log("Data", this.state.dataArr);
       localStorage.setItem("LocalDataArr", JSON.stringify(this.state.dataArr));
-      //var retrievedData = localStorage.getItem("LocalDataArr");
-      //items = JSON.parse(retrievedData);
-
-      // items = items.filter((item) => item.id == 2);
     });
   }
-  //   handleChange = (e) => {
-  //     this.props.onUpdate(e.target.value);
-  //     this.setState({ fieldVal: e.target.value });
-  //   };
-
   taskList() {
     if (this.state.dataArr.length > 0) {
       return this.state.dataArr.map(function (currentDataArr, i) {
@@ -108,11 +154,12 @@ export default class task extends Component {
       );
     }
   }
+
   render() {
     return (
       <div>
-        <h1>Task</h1>
-        <table className="table">
+        <AddNewTask />
+        <table className="table" id="taskTbl">
           <thead>
             <tr>
               <th>Id</th>
